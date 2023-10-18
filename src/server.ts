e2796238e513d779e7daaf44683250d1801f16dd
nischal1101 <kaflenk123@gmail.com>
2023-10-18 16:04:39 +0545

    make user route

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,18 @@
 import express from "express";
 import { PORT } from "./config";
 import db from "./db/connect";
+import userRouter from "./routes/user.routes";
 const app = express();
+
+app.use("/api/user",userRouter);
 
 db()
   .then(() => {
     console.log("Database connected");
     app.listen(PORT, () => {
-  console.log(`Server is running at port ${PORT}`);
-});
+      console.log(`Server is running at port ${PORT}`);
+    });
   })
   .catch((e) => {
     console.log(e);
   });
-
