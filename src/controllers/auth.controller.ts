355313c212d3a83cf7d9d2f500ae0db0bfae9524
nischal1101 <kaflenk123@gmail.com>
2023-10-19 17:28:25 +0545

    create sign in route

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -3,6 +3,8 @@ import bcrypt from "bcrypt";
 import User from "../models/user.model";
 import CustomErrorHandler from "../utils/CustomErrorHandler";
 import ReturnResponse from "../interface/returnResponse";
+import jwt from "jsonwebtoken";
+import { PUBLIC_KEY } from "../config";
 
 interface SignupRequestBody {
   password: string;
@@ -34,6 +36,44 @@ export async function signup(
     const error = new CustomErrorHandler(
       "validation failed " + err.message,
       422
+    );
+    next(error);
+  }
+}
+export async function signin(req: Request, res: Response, next: NextFunction) {
+  let returnResponse: ReturnResponse;
+  try {
+    const { email, password } = req.body;
+    const finduser = await User.findOne({ email }).select(
+      "-password -createdAt -updatedAt"
+    );
+    if (!finduser) {
+      const err = new CustomErrorHandler("Unauthorized", 401);
+      return next(err);
+    }
+    const match = await bcrypt.compare(password, finduser.password);
+    if (!match) {
+      const err = new CustomErrorHandler("Unauthorized", 401);
+      return next(err);
+    }
+    const accessToken = jwt.sign(
+      { _id: finduser._id, email: finduser.email },
+      PUBLIC_KEY!
+    );
+
+    returnResponse = {
+      status: "success",
+      message: "User successfully logged in",
+      data: { finduser },
+    };
+    return res
+      .cookie("access_token", accessToken, { httpOnly: true })
+      .status(200)
+      .json(returnResponse);
+  } catch (err: any) {
+    const error = new CustomErrorHandler(
+      "Internal Server Error " + err.message,
+      500
     );
     next(error);
   }
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,7 +1,8 @@
-import {Router} from "express";
-import { signup } from "../controllers/auth.controller";
-const router=Router();
+import { Router } from "express";
+import { signin, signup } from "../controllers/auth.controller";
+const router = Router();
 
-router.route("/signup").post(signup)
+router.route("/signup").post(signup);
+router.route("/signin").post(signin);
 
 export default router;
